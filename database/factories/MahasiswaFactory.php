e18370ec6bfea0aa6f64c247178f9f15ccb9d6c7
Antonius Yonathan <atomicbomber.email@gmail.com>
2018-09-13 17:44:09 +0700

    Random NIMs now have the same amount of trailing digits.

--- database/factories/MahasiswaFactory.php
+++ database/factories/MahasiswaFactory.php
@@ -4,6 +4,6 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Mahasiswa::class, function (Faker $faker) {
     return [
-        'NIM' => 'D' . $faker->unique->randomNumber(6)
+        'NIM' => 'D' . $faker->unique->randomNumber(6, TRUE)
     ];
 });
