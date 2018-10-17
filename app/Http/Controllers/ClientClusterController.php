8f9efed94954d2e7fef85b9b0778ff90a4ff6029
Antonius Yonathan <atomicbomber.email@gmail.com>
2018-10-17 09:09:11 +0700

    Mahasiswas can now login and see which clusters they belong to.

--- app/Http/Controllers/ClientClusterController.php
+++ app/Http/Controllers/ClientClusterController.php
@@ -3,11 +3,29 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ClientClusterController extends Controller
 {
     public function index()
     {
-        return view('client.cluster.index');
+        $mahasiswa_id = auth()->user()->mahasiswa->id;
+
+        $mahasiswa_clusters = DB::select("
+            SELECT cluster, ganjil_genap, tahun_mulai, tahun_selesai, \"IPK\", \"IPS\" FROM nilais
+                JOIN tahun_ajarans ON tahun_ajarans.id = nilais.tahun_ajaran_id
+                WHERE
+                    mahasiswa_id = $mahasiswa_id
+                ORDER BY
+                    tahun_selesai DESC,
+                    ganjil_genap DESC
+        ");
+
+        // $mahasiswa_genap_clusters = collect($mahasiswa_genap_clusters)
+        //     ->groupBy(function($nilai) {
+        //         return $nilai->tahun_mulai . '-' . $nilai->tahun_selesai;
+        //     });
+
+        return view('client.cluster.index', compact('mahasiswa_clusters'));
     }
 }
--- resources/views/client/cluster/index.blade.php
+++ resources/views/client/cluster/index.blade.php
@@ -19,6 +19,41 @@
             </div>
         </article>
         @endif
+
+        <div class="card is-inline-block">
+            <div class="card-header">
+                <h1 class="card-header-title">
+                    <span class="icon mr-1">
+                        <i class="fa fa-list"></i>
+                    </span>
+                    Daftar Kluster Anda
+                </h1>
+            </div>
+            <div class="card-content">
+                <table class="table is-striped">
+                    <thead>
+                        <tr>
+                            <th> Tahun Ajaran </th>
+                            <th> Ganjil / Genap </th>
+                            <th> IPK </th>
+                            <th> IPS </th>
+                            <th> Kluster </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($mahasiswa_clusters as $cluster)
+                        <tr>
+                            <td> {{ $cluster->tahun_mulai }}-{{ $cluster->tahun_selesai }} </td>
+                            <td> {{ $cluster->ganjil_genap }} </td>
+                            <td> {{ $cluster->IPK ?? '-' }} </td>
+                            <td> {{ $cluster->IPS ?? '-' }} </td>
+                            <td> {{ $cluster->cluster ?? '-' }} </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
     </div>
 </div>
 @endsection
